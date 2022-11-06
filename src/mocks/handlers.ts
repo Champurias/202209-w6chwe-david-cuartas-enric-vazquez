efb26e99c440678000dc66bcc634a8bc736615bc
Enric Vazquez <vspevc@gmail.com>
2022-11-06 21:30:55 +0100

    Modify api url in msw handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,7 +3,7 @@ import { rest } from "msw";
 const apiUrl = process.env.REACT_APP_API_URL as string;
 
 export const handlers = [
-  rest.get(apiUrl, (req, res, ctx) => {
+  rest.get(`${apiUrl}`, (req, res, ctx) => {
     return res(ctx.status(200));
   }),
 ];
